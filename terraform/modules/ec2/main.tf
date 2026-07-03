fb3da567c2d306509fd333bad0fa5acc84e6a268
Rakesh Ku Patra <rakeshkupatra3@gmail.com>
2026-07-03 22:43:22 +0530

    fix: refactor Docker repository setup in Ansible and Terraform using native modules and evaluation

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -85,7 +85,7 @@ resource "aws_instance" "app" {
               mkdir -p /etc/apt/keyrings
               curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
               chmod a+r /etc/apt/keyrings/docker.asc
-              echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \$(. /etc/os-release && echo \$VERSION_CODENAME) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+              echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
               apt-get update -y
               apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
               systemctl enable --now docker
