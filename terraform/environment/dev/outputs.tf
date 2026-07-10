1cf9092100cb5839cf63a4138af9ff6914c5b45b
Rakesh Ku Patra <rakeshkupatra3@gmail.com>
2026-07-10 23:53:50 +0530

    feat: implement dynamic security group whitelisting for Vault and SSH in CI/CD

--- terraform/environment/dev/outputs.tf
+++ terraform/environment/dev/outputs.tf
@@ -12,3 +12,9 @@ output "public_dns" {
   description = "The public DNS of the EC2 instance"
   value       = module.ec2.public_dns
 }
+
+output "security_group_id" {
+  description = "The ID of the EC2 security group"
+  value       = module.ec2.security_group_id
+}
+
--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -12,3 +12,9 @@ output "public_dns" {
   description = "The public DNS of the EC2 instance"
   value       = aws_instance.app.public_dns
 }
+
+output "security_group_id" {
+  description = "The ID of the EC2 security group"
+  value       = aws_security_group.ec2.id
+}
+
